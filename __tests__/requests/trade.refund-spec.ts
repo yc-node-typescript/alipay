42aaa1d94b29eeed7cec80893460fd29d6bab5f4
Yu Chen <yu.chen@live.ie>
2018-05-16 14:18:16 +0800

    修复退款

--- __tests__/requests/trade.refund-spec.ts
+++ __tests__/requests/trade.refund-spec.ts
@@ -4,13 +4,13 @@ test('Should set simple bizContent', () => {
   const req = new TradeRefundRequest();
   req.setBizContent({
     out_trade_no: 'test001',
-    refund_amount: 10,
+    refund_amount: '10',
   });
   expect(req.method).toBe('alipay.trade.refund');
   expect(req.data.bizContent).toBe(
     JSON.stringify({
       out_trade_no: 'test001',
-      refund_amount: 10,
+      refund_amount: '10',
     })
   );
 });
